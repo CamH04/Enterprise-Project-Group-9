6f6b44132744d566aa1905371e7a669f9da9d804
CamH04 <cameronahaynes04@gmail.com>
2025-03-04 11:13:13 +0000

    added raw crop website before meeting

---
+++ A6 - Corporate Identity Marketing strategy website design/websiteA6/scrappedwebsite/site/src/App.js
@@ -0,0 +1,16 @@
+import Home from './Home.js';
+import HomeBody from './homebody.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container-home">
+        <Home />
+        <HomeBody />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ A6 - Corporate Identity Marketing strategy website design/websiteA6/scrappedwebsite/site/src/homebody.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function HomeBody() {
+  return (
+      <div className="container-home">
+        <h1 className="title"> Mission Statement</h1>
+        <h3 className="title">" empowering individuals with inclusive, accessible, and evidence-based mental health support, ensuring alignment with NHS resources and digital well-being solutions "</h3>
+        <h1 className="title">Our Values</h1>
+        <h3 className="title">• Accessibility: Simple, inclusive, and easy to navigate for all users.
+    • Trust: NHS-backed credibility.
+    • Engagement: Interactive features that encourage active self-care.</h3>
+      </div>
+  );
+}
+
+export default HomeBody;
